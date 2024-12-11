b33dba7a72339082e61d0cb21d17ae637f49c48c
Rohankc073 <rohankc073@gmail.com>
2024-12-11 21:17:20 +0545

    Doctor

---
+++ Backend/models/doctor.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const DoctorSchema = new mongoose.Schema({
+    name : {type : String ,required : true},
+    email : {type: String, required: true, unique: true},
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    age: { type: Number, required: true },
+    gender: { type: String, required: true },
+    
+
+})
